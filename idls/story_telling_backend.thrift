5b3044e4ef589213826e07911297c443ef8ec896
huqiyi <1523332452@qq.com>
2023-07-23 16:10:07 +0800

    修改编译脚本

---
+++ idls/story_telling_backend.thrift
@@ -0,0 +1,103 @@
+namespace go story_telling_backend
+
+struct Pagination {
+    1: required i32 PageSize (api.body="page_size") // 大于0，默认值1
+    2: required i32 PageNo (api.body="page_no") // 大于0，默认值10
+}
+
+struct SearchNovelReq {
+    1: optional string CustomValue (api.body="custom_value") // 非必填，不传不用于筛选
+    2: optional list<string> Labels (api.body="labels") // 非必填，不传或长度为空不用于筛选
+    3: optional Pagination pagination (api.body="pagination")
+}
+
+struct SearchNovelItem {
+    1: required i64 ID (api.body="id") // 小说id
+    2: required string CoverUrl (api.body="cover_url") // 封面图url, 这里用编码后的图片文件还是url呢？
+    3: required string NovelName (api.body="novel_name") // 小说名字
+    4: required string AuthorName (api.body="author_name") // 作者名字
+    5: required list<string> Labels (api.body="labels") // 小说标签
+    6: required list<string> Describes (api.body="describes") // 一些七七八八的描述
+}
+
+struct SearchNovelData {
+    1: required SearchNovelItem Items (api.body="items")
+    2: required i64 Total (api.body="total")
+}
+
+struct SearchNovelResp {
+    1: required i32 Code (api.body="code")
+    2: required string Message (api.body="message")
+    3: optional SearchNovelData Data (api.body="data")
+}
+
+struct GetNovelDetailReq {
+}
+
+struct GetNovelDetailResp {
+}
+
+struct GetNovelChapterTitleListReq {
+    1: required string NovelID (api.path="novel_id")
+    2: required i32 PageSize (api.body="page_size") // 大于0，默认值1
+    3: required i32 PageNo (api.body="page_no") // 大于0，默认值10
+}
+
+struct GetNovelChapterTitleItem {
+    1: required i64 ID (api.body="id") // 章节id
+    2: required string Title (api.body="title") // 章节标题
+}
+
+struct GetNovelChapterTitleListData {
+    1: required GetNovelChapterTitleItem Items (api.body="items")
+    2: required i64 Total (api.body="total")
+}
+
+struct GetNovelChapterTitleListResp {
+    1: required i32 Code (api.body="code")
+    2: required string Message (api.body="message")
+    3: optional GetNovelChapterTitleListData Data (api.body="data")
+}
+
+struct GetNovelTellingConfigReq {
+    1: required string NovelID (api.path="novel_id")
+}
+
+struct GetNovelTellingConfigData { // 这里应该枚举，枚举值待定
+    1: required list<string> Speed (api.body="speed") // 播放速度
+    2: required list<string> Timbre (api.body="timbre") // 音色
+}
+
+struct GetNovelTellingConfigResp {
+    1: required i32 Code (api.body="code")
+    2: required string Message (api.body="message")
+    3: optional GetNovelTellingConfigData Data (api.body="data")
+}
+
+struct GetChapterDetailReq {
+    1: required string NovelID (api.path="novel_id")
+    2: required string ChapterID (api.path="chapter_id")
+}
+
+struct GetChapterDetailData {
+    1: required string Title (api.body="title") // 章节标题
+    2: required string Frontend (api.body="frontend")
+    3: required i64 Duration (api.body="duration") // 音频时长, ms
+    4: required binary Audio (api.body="audio") // 音频，mp3格式，需要decode
+}
+
+struct GetChapterDetailResp {
+    1: required i32 Code (api.body="code")
+    2: required string Message (api.body="message")
+    3: optional GetChapterDetailData Data (api.body="data")
+}
+
+service NovelBackendService {
+    // novel
+    SearchNovelResp SearchNovel(1: SearchNovelReq request) (api.post="/story_telling/api/v1/novel/search");
+    GetNovelDetailResp GetNovelDetail(1: GetNovelDetailReq request) (api.get="/story_telling/api/v1/novel/detailvel_id");
+    GetNovelTellingConfigResp GetNovelTellingConfig(1: GetNovelTellingConfigReq request) (api.get="/story_telling/api/v1/telling/configvel_id");
+    // chapter
+    GetNovelChapterTitleListResp GetNovelChapterTitle(1: GetNovelChapterTitleListReq request) (api.get="/story_telling/api/v1/chapter/title_listvel_id");
+    GetChapterDetailResp GetChapterDetail(1: GetChapterDetailReq request) (api.get="/story_telling/api/v1/chapter/detailvel_id/:chapter_id");
+}

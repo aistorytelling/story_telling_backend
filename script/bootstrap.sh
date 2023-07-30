552b85877473aaf7e9629e3d3a4cf04630e88670
huqiyi <1523332452@qq.com>
2023-07-30 13:49:07 +0800

    bootstrap add nohup

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -3,4 +3,4 @@
 CURDIR=$(cd $(dirname $0); pwd)
 BinaryName=story_telling_backend
 echo "$CURDIR/bin/${BinaryName}"
-exec $CURDIR/bin/${BinaryName}
+nohup $CURDIR/bin/${BinaryName}  > out.log 2>&1 &
